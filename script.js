ac071223006247938bc89b81ef9603b5688132a4
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-09-08 18:45:00 -0500

    titles up & added button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createEleent ("h2");
 h2.textContent= "This content added by JavaScript";
 document.querySelector ("body").appendChild(h2);
+
+const input = document.getElementById('button');
+function addingEventListener() {
+input.addEventListener('click', function() {
+ alert('I was clicked!');
+});
+}
