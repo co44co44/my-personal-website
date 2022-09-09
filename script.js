50d8dad02a119e062e654029e080b13e1e0cc873
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-09-08 20:12:00 -0500

    Finish Website 1

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const h2 = document.createEleent ("h2");
 h2.textContent= "This content added by JavaScript";
-document.querySelector ("body").appendChild(h2);
+//document.querySelector ("body").appendChild(h2);
 
 const input = document.getElementById('button');
-function addingEventListener() {
-input.addEventListener('click', function() {
- alert('I was clicked!');
-});
-}
+input.addEventListener('click', alert('I was clicked!');
+
